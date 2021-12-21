8a3de587a27367b51f8f228f581d10fe861540bd
Tuomas Liinamaa <TLii@users.noreply.github.com>
2021-12-21 18:22:15 +0200

    Update

---
+++ toMaatti/toMaatti/toMaatti/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace toMaatti
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void nappiaPainettu(object sender, EventArgs e)
+        {
+            Ajastin.aloitaAjastus();
+        }
+    }
+
+    public static class Ajastin
+    {
+        public static System.Timers.Timer tomaattiAjastin;
+        public static void aloitaAjastus()
+        {
+            tomaattiAjastin = new System.Timers.Timer(1000);
+
+        }
+    }
+}
